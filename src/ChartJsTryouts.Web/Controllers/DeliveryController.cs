2095dd842e38ec90226f2f9ed29457a2bc85d3d3
corirnx <47rc91@gmail.com>
2020-06-06 10:52:11 +0200

    inital simple clickable bar chart

---
+++ src/ChartJsTryouts.Web/Controllers/DeliveryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace ChartJsTryouts.Web.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        public IActionResult OfDay(string day)
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ChartJsTryouts.Web/Models/DeliveryOverviewViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ChartJsTryouts.Lib.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ChartJsTryouts.Web.Models
+{
+    public class DeliveryOverviewViewModel
+    {
+        public OverviewDay[] Days { get; set; }
+
+        public Dictionary<DateTime, int> PeterSmith { get; set; }
+        public Dictionary<DateTime, int> RobertPope { get; set; }
+        public Dictionary<DateTime, int> SashaHerrman { get; set; }
+        public Dictionary<DateTime, int> MonicaSnyder { get; set; }
+        public Dictionary<DateTime, int> BeatrixZimmermann { get; set; }
+
+        public DeliveryOverviewViewModel()
+        {
+            PeterSmith = new Dictionary<DateTime, int>();
+            RobertPope = new Dictionary<DateTime, int>();
+            SashaHerrman = new Dictionary<DateTime, int>();
+            MonicaSnyder = new Dictionary<DateTime, int>();
+            BeatrixZimmermann = new Dictionary<DateTime, int>();
+        }
+    }
+
+    public class OverviewDay
+    {
+        public DateTime DeliveryCreation { get; set; }
+
+        public Delivery[] PeterSmith { get; set; }
+        public Delivery[] RobertPope { get; set; }
+        public Delivery[] SashaHerrman { get; set; }
+        public Delivery[] MonicaSnyder { get; set; }
+        public Delivery[] BeatrixZimmermann { get; set; }
+
+        public Delivery[] GetAll
+        {
+            get
+            {
+                var list = new List<Delivery>();
+                list.AddRange(PeterSmith);
+                list.AddRange(RobertPope);
+                list.AddRange(SashaHerrman);
+                list.AddRange(MonicaSnyder);
+                list.AddRange(BeatrixZimmermann);
+
+                return list.ToArray();
+            }
+        }
+    }
+}
