a7373be04eab5322de37e4b26fb8c86c2ac1aa0d
corirnx <47rc91@gmail.com>
2020-06-06 10:08:57 +0200

    add random generation of some data

---
+++ src/ChartJsTryouts.Lib/DeliveryManager.cs
@@ -0,0 +1,57 @@
+﻿using ChartJsTryouts.Lib.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ChartJsTryouts.Lib
+{
+    public class DeliveryManager
+    {
+        string[] _deliverer;
+        Random _random;
+
+        public DeliveryManager()
+        {
+            _deliverer = new[] { "Peter Smith", "Robert Pope", "Sasha Herman", "Monika Snyder", "Beatrix Zimmermanm" };
+            _random = new Random();
+        }
+
+        public Delivery[] GetDeliveriesOfWeek()
+        {
+            var totalDeliveries = new List<Delivery>();
+
+            for (var d = 0; d < 7; d++)
+            {
+                var date = DateTime.Now.AddDays(-d);
+
+                var randomAmount = _random.Next(4, 30);
+
+                for (var r = 0; r < randomAmount; r++)
+                    totalDeliveries.Add(new Delivery
+                    {
+                        Id = r,
+                        Creation = date,
+                        Deliverer = GetNextDeliverer(),
+                        DeliveryDuration = GetNextDeliveryDuration()
+                    });
+            }
+
+            return totalDeliveries.ToArray();
+        }
+
+        string GetNextDeliverer()
+        {
+            var r = _random.Next(0, (_deliverer.Length - 1));
+            return _deliverer[r];
+        }
+
+        TimeSpan GetNextDeliveryDuration()
+        {
+            return new TimeSpan(NextRandom(23), NextRandom(59), NextRandom(59));
+        }
+
+        int NextRandom(int max)
+        {
+            return _random.Next(0, max);
+        }
+    }
+}
---
+++ src/ChartJsTryouts.Lib/Models/Delivery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChartJsTryouts.Lib.Models
+{
+    public class Delivery
+    {
+        public int Id { get; set; }
+        public DateTime Creation { get; set; }
+        public TimeSpan DeliveryDuration { get; set; }
+        public string Deliverer { get; set; }
+
+    }
+}
