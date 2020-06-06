28e1a47dfeab9a9edb3859d0c16c2f70c45824b0
corirnx <47rc91@gmail.com>
2020-06-06 14:19:16 +0200

    cleanup

--- src/ChartJsTryouts.Web/Controllers/ClickableBarChart/ClickableBarChartController.cs
+++ src/ChartJsTryouts.Web/Controllers/ClickableBarChart/ClickableBarChartController.cs
@@ -1,8 +1,6 @@
 ﻿using ChartJsTryouts.Lib;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ChartJsTryouts.Web.Controllers.ClickableBarChart
 {
@@ -17,57 +15,12 @@ namespace ChartJsTryouts.Web.Controllers.ClickableBarChart
 
         public IActionResult ClickableBar()
         {
+            // get some statistic data for bar chart example
             var deliveriers = _deliveryManager.GetDeliveriesOfWeek();
 
-            var vm = new DeliveryOverviewViewModel();
+            var weekViewModel = new WeekViewViewModel(deliveriers);         
 
-            var groupedDeliveries = deliveriers.GroupBy(d => d.Creation, (key, g) => new { Creation = key, Deliveries = g.ToList() })
-                .ToArray();
-
-            var list = new List<OverviewDay>();
-            foreach (var day in groupedDeliveries)
-            {
-                var ob = new OverviewDay
-                {
-                    DeliveryCreation = day.Creation,
-                    DeliveriesOfDay = new Dictionary<string, Lib.Models.Delivery[]>()
-                };
-
-                var deliverersDistinct = day.Deliveries.Select(d => d.Deliverer).Distinct().ToArray();
-
-                foreach (var deliverer in deliverersDistinct)
-                {
-                    ob.DeliveriesOfDay.Add(deliverer, day.Deliveries.Where(d => d.Deliverer.Equals(deliverer)).ToArray());
-                }
-
-                list.Add(ob);
-            }
-
-            vm.Days = list.ToArray();
-
-            var deliverers = vm.Days.Select(d => d.DeliveriesOfDay).SelectMany(d => d.Keys).Distinct().ToArray();
-
-            foreach (var deliverer in deliverers)
-            {
-                var data = vm.Days.SelectMany(t => t.DeliveriesOfDay.Where(dd => dd.Key == deliverer).SelectMany(v => v.Value)).ToArray();
-                var groupd = data.GroupBy(d => d.Creation, (key, g) => new { Creation = key, Deliveries = g.ToArray() }).ToArray();
-
-                var list2 = new List<DelivererAmountPerDay>();
-                foreach (var g in groupd)
-                {
-                    var o = new DelivererAmountPerDay
-                    {
-                        Day = g.Creation,
-                        Amount = g.Deliveries.Length
-                    };
-
-                    list2.Add(o);
-                }
-
-                vm.DelivererAmountPerDay.Add(deliverer, list2.ToArray());
-            }
-
-            return View("DeliveryOverview", vm);
+            return View("DeliveryOverview", weekViewModel);
         }
 
 
---
+++ src/ChartJsTryouts.Web/Controllers/ClickableBarChart/WeekViewViewModel.cs
@@ -0,0 +1,105 @@
+﻿using ChartJsTryouts.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChartJsTryouts.Web.Controllers.ClickableBarChart
+{
+    public class WeekViewViewModel
+    {
+        public Dictionary<string, DeliveriesCountOfDay[]> DelivererWeekStatistic { get; set; }
+        public DateTime[] Days { get; set; }
+
+        public WeekViewViewModel(Delivery[] deliveriers)
+        {
+            DelivererWeekStatistic = new Dictionary<string, DeliveriesCountOfDay[]>();
+
+            // group deliveries per day
+            var deliveriesPerDay = deliveriers
+                .GroupBy(d => d.Creation,
+                    (key, g) => new DayGroup { Day = key, Deliveries = g.ToArray() })
+                .ToArray();
+
+            var allDeliveriesPerDays = PrepareDeliveriesPerDayandDeliverer(deliveriesPerDay);
+
+            // add supported days
+            Days = allDeliveriesPerDays.Select(i => i.DeliveryCreation).Distinct().ToArray();
+
+            MapDataToDisplay(allDeliveriesPerDays);
+        }
+
+        DeliveriesOfDay[] PrepareDeliveriesPerDayandDeliverer(DayGroup[] deliveriesPerDay)
+        {
+            var deliveriesPerDayList = new List<DeliveriesOfDay>();
+
+            foreach (var dayDeliveries in deliveriesPerDay)
+                deliveriesPerDayList.Add(FillForDay(dayDeliveries.Day, dayDeliveries.Deliveries));
+
+            return deliveriesPerDayList.ToArray();
+        }
+
+        DeliveriesOfDay FillForDay(DateTime day, Delivery[] deliveries)
+        {
+            var deliveriesForDay = new DeliveriesOfDay(day);
+
+            var deliverers = deliveries.Select(d => d.Deliverer).Distinct().ToArray();
+
+            foreach (var deliverer in deliverers)
+                FillForDay(deliveriesForDay, deliverer, deliveries.Where(d => d.Deliverer.Equals(deliverer)).ToArray());
+
+            return deliveriesForDay;
+        }
+
+
+        void FillForDay(DeliveriesOfDay deliveriesForDay, string deliverer, Delivery[] deliveries)
+        {
+            deliveriesForDay.DeliveriesPerDeliverer.Add(deliverer, deliveries);
+        }
+
+        void MapDataToDisplay(DeliveriesOfDay[] allDeliveriesPerDays)
+        {
+            var deliverers = allDeliveriesPerDays
+                .Select(d => d.DeliveriesPerDeliverer)
+                .SelectMany(d => d.Keys)
+                .Distinct()
+                .ToArray();
+
+            foreach (var deliverer in deliverers)
+            {
+                var deliveriesOfDeliverer = allDeliveriesPerDays
+                    .SelectMany(t => t.DeliveriesPerDeliverer.Where(dd => dd.Key == deliverer).SelectMany(v => v.Value))
+                    .ToArray();
+
+                var groupedDeliveries = deliveriesOfDeliverer
+                    .GroupBy(d => d.Creation, (key, g) => new { Creation = key, Deliveries = g.ToArray() })
+                    .ToArray();
+
+                DelivererWeekStatistic.Add(deliverer,
+                    groupedDeliveries.Select(g => new DeliveriesCountOfDay { Day = g.Creation, Amount = g.Deliveries.Length }).ToArray());
+            }
+        }
+    }
+
+    public class DeliveriesCountOfDay
+    {
+        public DateTime Day { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class DeliveriesOfDay
+    {
+        public DateTime DeliveryCreation { get; set; }
+        public Dictionary<string, Delivery[]> DeliveriesPerDeliverer { get; set; }
+        public DeliveriesOfDay(DateTime creation)
+        {
+            DeliveryCreation = creation;
+            DeliveriesPerDeliverer = new Dictionary<string, Delivery[]>();
+        }
+    }
+
+    class DayGroup
+    {
+        public DateTime Day { get; set; }
+        public Delivery[] Deliveries { get; set; }
+    }
+}
